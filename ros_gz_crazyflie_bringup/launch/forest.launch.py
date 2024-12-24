c0bfaaa2726a1a0772b9692fde4b9ac9f8acb4ec
noshluk2 <noshluk2@gmail.com>
2024-12-24 11:47:22 +0500

    fores world added

--- ros_gz_crazyflie_bringup/launch/forest.launch.py
+++ ros_gz_crazyflie_bringup/launch/forest.launch.py
@@ -1,32 +1,44 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-
+import xacro
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # Configure ROS nodes for launch
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
     # Setup project paths
     pkg_project_bringup = get_package_share_directory('ros_gz_crazyflie_bringup')
     pkg_project_gazebo = get_package_share_directory('ros_gz_crazyflie_gazebo')
     pkg_ros_gz_sim = get_package_share_directory('ros_ign_gazebo')
-
-    # Set environment variable for Gazebo model path
-    gz_model_path = '/home/luqman/robotisim_ws/src/dev_robotisim/crazyflie-simulation/simulator_files/gazebo'
-
-    set_gz_model_path = SetEnvironmentVariable(
-        name='IGN_GAZEBO_RESOURCE_PATH',
-        value=gz_model_path
-    )
+    gz_model_path = os.getenv('IGN_GAZEBO_RESOURCE_PATH')
 
     # Load the SDF file from "description" package
-    sdf_file = os.path.join(gz_model_path, 'crazyflie', 'model.sdf')
+    sdf_file  =  os.path.join(gz_model_path, 'crazyflie', 'model.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
@@ -37,8 +49,15 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'house_world.sdf -r'
+            'forest_world.sdf -r'
         ])}.items(),
+    )
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', os.path.join(pkg_project_bringup, 'rviz', 'sensor_viz.rviz')],
+        output='screen'
     )
 
     bridge = Node(
@@ -47,8 +66,15 @@ def generate_launch_description():
         parameters=[{
             'config_file': os.path.join(pkg_project_bringup, 'config', 'ros_gz_crazyflie_bridge.yaml'),
         }],
+
         output='screen'
     )
+
+    map_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'crazyflie/odom'])
 
     control = Node(
         package='ros_gz_crazyflie_control',
@@ -63,8 +89,12 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        set_gz_model_path,  # Automatically set the environment variable
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
         gz_sim,
+        map_static_tf,
         bridge,
-        control
-    ])
+        rviz2_node,
+        control        ])
--- ros_gz_crazyflie_bringup/launch/house.launch.py
+++ ros_gz_crazyflie_bringup/launch/house.launch.py
@@ -1,9 +1,24 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -12,21 +27,17 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # Configure ROS nodes for launch
+
     # Setup project paths
     pkg_project_bringup = get_package_share_directory('ros_gz_crazyflie_bringup')
     pkg_project_gazebo = get_package_share_directory('ros_gz_crazyflie_gazebo')
     pkg_ros_gz_sim = get_package_share_directory('ros_ign_gazebo')
+    gz_model_path = os.getenv('IGN_GAZEBO_RESOURCE_PATH')
 
-    # Set environment variable for Gazebo model path
-    gz_model_path = '/home/luqman/robotisim_ws/src/dev_robotisim/crazyflie-simulation/simulator_files/gazebo'
-
-    set_gz_model_path = SetEnvironmentVariable(
-        name='IGN_GAZEBO_RESOURCE_PATH',
-        value=gz_model_path
-    )
 
     # Load the SDF file from "description" package
-    sdf_file = os.path.join(gz_model_path, 'crazyflie', 'model.sdf')
+    sdf_file  =  os.path.join(gz_model_path, 'crazyflie', 'model.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
@@ -37,7 +48,7 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'crazyflie_world.sdf -r'
+            'house_world.sdf -r'
         ])}.items(),
     )
 
@@ -47,6 +58,7 @@ def generate_launch_description():
         parameters=[{
             'config_file': os.path.join(pkg_project_bringup, 'config', 'ros_gz_crazyflie_bridge.yaml'),
         }],
+
         output='screen'
     )
 
@@ -61,10 +73,16 @@ def generate_launch_description():
             {'max_ang_z_rate': 0.4},
         ]
     )
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', os.path.join(pkg_project_bringup, 'rviz', 'sensor_viz.rviz')],
+        output='screen'
+    )
 
     return LaunchDescription([
-        set_gz_model_path,  # Set the environment variable
         gz_sim,
         bridge,
-        control
-    ])
+        rviz2_node,
+        control        ])
