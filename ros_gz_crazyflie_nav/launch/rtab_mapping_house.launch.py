ca402284c9225e4bcb70b43ca0aa932aeaf94cf7
Haroon Rasheed <haroon300@hotmail.com>
2024-12-31 20:53:20 +0500

    init commit

---
+++ ros_gz_crazyflie_nav/launch/rtab_mapping_house.launch.py
@@ -0,0 +1,89 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    qos = LaunchConfiguration('qos')
+    localization = LaunchConfiguration('localization')
+
+    parameters={
+          'frame_id':'crazyflie/body',
+          'odom_frame_id': 'odom',
+          'use_sim_time':use_sim_time,
+          'subscribe_depth':True,
+          'use_action_for_goal':True,
+          'qos_image':qos,
+          'qos_imu':qos,
+        #   'Reg/Force3DoF':'true',
+        #   'Optimizer/GravitySigma':'0' # Disable imu constraints (we are already in 2D)
+    }
+
+    remappings=[
+        ('/odom', '/crazyflie/odom'),
+        ('rgb/image', '/camera/image'),
+        ('rgb/camera_info', '/camera/camera_info'),
+        ('depth/image', '/camera/depth')
+    ]
+
+    # Include the demo.launch.py from ros_gz_crazyflie_bringup package
+    bringup_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros_gz_crazyflie_bringup'),
+                'launch',
+                'crazyflie_house.launch.py'
+            )
+        )
+    )
+
+
+    return LaunchDescription([
+
+        # Launch arguments
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'qos', default_value='2',
+            description='QoS used for input sensor topics'),
+
+        DeclareLaunchArgument(
+            'localization', default_value='false',
+            description='Launch in localization mode.'),
+
+        # Nodes to launch
+
+        # SLAM mode:
+        Node(
+            condition=UnlessCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=['-d']), # This will delete the previous database (~/.ros/rtabmap.db)
+
+        # Localization mode:
+        Node(
+            condition=IfCondition(localization),
+            package='rtabmap_slam', executable='rtabmap', output='screen',
+            parameters=[parameters,
+              {'Mem/IncrementalMemory':'False',
+               'Mem/InitWMWithAllNodes':'True'}],
+            remappings=remappings),
+
+        Node(
+            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+            parameters=[parameters],
+            remappings=remappings),
+
+
+        # Include bringup_launch_file
+        bringup_launch_file,
+    ])
